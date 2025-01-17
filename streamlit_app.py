dde309d5a693effc55d17f951091ac0e8a35f560
developer7-ai <sarawin.ibm.id@gmail.com>
2025-01-17 11:57:28 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
+import numpy as np
+import cv2
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def main():
+    st.set_page_config(
+    page_title="G-AI-OCR")
+    st.title("OCR")
+    st.header("Choose an image")
+    uploaded_file = st.file_uploader("Upload Your Image",type=["jpg","png"])
+    if uploaded_file is not None:
+        try:
+            img = np.array(Image.open(uploaded_file))
+            st.image(img)
+            text = pytesseract.image_to_string(img)
+            st.write(text)
+
+        
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
